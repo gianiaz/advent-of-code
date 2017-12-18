72a7740a80363f838ec2ea2342777fe2d6437128
Alessandro Lai <alessandro.lai85@gmail.com>
2017-12-18 11:42:58 +0100

    Implement handling second part of solution

--- solve.php
+++ solve.php
@@ -1,5 +1,7 @@
 <?php
 
+use Jean85\AdventOfCode\SecondPartSolutionInterface;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $day = (int) $argv[1];
@@ -8,10 +10,21 @@ if (! is_int($day)) {
     throw new \InvalidArgumentException('Invalid day: ' . $day);
 }
 
-$solution = new \Jean85\AdventOfCode\Day1\Day1Solution();
+$className = \sprintf('\Jean85\AdventOfCode\Day%s\Day%sSolution', $day, $day);
+if (! class_exists($className)) {
+    throw new \InvalidArgumentException('Missing solution class: ' . $className);
+}
+
+$solution = new $className();
 
 echo 'Solution for day ' . $day . PHP_EOL . PHP_EOL;
 
 print_r($solution->solve());
+
+if ($solution instanceof SecondPartSolutionInterface) {
+    echo 'Second part of the solution for day ' . $day . PHP_EOL . PHP_EOL;
+
+    print_r($solution->solveSecondPart());
+}
 
 echo PHP_EOL;
---
+++ src/SecondPartSolutionInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Jean85\AdventOfCode;
+
+interface SecondPartSolutionInterface
+{
+    public function solveSecondPart();
+}
