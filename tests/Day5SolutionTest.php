67437d0a4878edeae9ebadf4a6686876592218e0
Alessandro Lai <alessandro.lai85@gmail.com>
2017-12-20 19:31:12 +0100

    Solve day 5, part 2: answer is too low?

--- tests/Day5SolutionTest.php
+++ tests/Day5SolutionTest.php
@@ -9,8 +9,17 @@ class Day5SolutionTest extends TestCase
 {
     public function testSolve()
     {
-        $solution = new Day5Solution([0, 3,  0,  1,  -3]);
+        $solution = new Day5Solution([0, 3, 0, 1, -3]);
 
         $this->assertSame(5, $solution->solve());
+        $this->assertSame([2, 5, 0, 1, -2], $solution->getInput());
+    }
+
+    public function testSolveSecondPart()
+    {
+        $solution = new Day5Solution([0, 3, 0, 1, -3]);
+
+        $this->assertSame(10, $solution->solveSecondPart());
+        $this->assertSame([2, 3, 2, 3, -1], $solution->getInput());
     }
 }
