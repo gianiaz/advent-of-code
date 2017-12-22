56f4219410d2e5296295771d1a51adaeb048b34b
Alessandro Lai <alessandro.lai85@gmail.com>
2017-12-23 00:25:11 +0100

    Solve Day 7, part 1

---
+++ src/Day7/Tower.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Jean85\AdventOfCode\Day7;
+
+class Tower
+{
+    /** @var string */
+    private $name;
+
+    /** @var int */
+    private $weight;
+
+    /** @var string[] */
+    private $supports;
+
+    /**
+     * Tower constructor.
+     * @param string $name
+     * @param int $weight
+     * @param string[] $supports
+     */
+    public function __construct(string $name, int $weight, array $supports)
+    {
+        $this->name = $name;
+        $this->weight = $weight;
+        $this->supports = $supports;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWeight(): int
+    {
+        return $this->weight;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getSupports(): array
+    {
+        return $this->supports;
+    }
+}
---
+++ tests/Day7SolutionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests;
+
+use Jean85\AdventOfCode\Day7\Day7Solution;
+use Jean85\AdventOfCode\Day7\Tower;
+use PHPUnit\Framework\TestCase;
+
+class Day7SolutionTest extends TestCase
+{
+    public function testSolve()
+    {
+        $solution = new Day7Solution($this->getTestInput());
+
+        $reversedList = $solution->getReverseTowerList();
+        $this->assertCount(13, $reversedList);
+
+        $filteredReversedList = [];
+        foreach ($reversedList as $towerName => $supportedBy) {
+            if ($supportedBy === []) {
+                $filteredReversedList[] = $towerName;
+            }
+        }
+
+        $this->assertSame(['tknk'], $filteredReversedList);
+        $this->assertSame('tknk', $solution->solve());
+    }
+
+    private function getTestInput()
+    {
+        return [
+            new Tower('pbga', 66, []),
+            new Tower('xhth', 57, []),
+            new Tower('ebii', 61, []),
+            new Tower('havc', 66, []),
+            new Tower('ktlj', 57, []),
+            new Tower('fwft', 72, ['ktlj', 'cntj', 'xhth']),
+            new Tower('qoyq', 66, []),
+            new Tower('padx', 45, ['pbga', 'havc', 'qoyq']),
+            new Tower('tknk', 41, ['ugml', 'padx', 'fwft']),
+            new Tower('jptl', 61, []),
+            new Tower('ugml', 68, ['gyxo', 'ebii', 'jptl']),
+            new Tower('gyxo', 61, []),
+            new Tower('cntj', 57, []),
+        ];
+    }
+}
