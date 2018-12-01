a328ba1322497852ebb616411269ccdc1d72102b
Alessandro Lai <alessandro.lai85@gmail.com>
2018-12-01 23:52:42 +0100

    Add second solution for day 1

--- tests/Xmas2018/Day1/Day1SolutionTest.php
+++ tests/Xmas2018/Day1/Day1SolutionTest.php
@@ -10,7 +10,7 @@ use PHPUnit\Framework\TestCase;
 class Day1SolutionTest extends TestCase
 {
     /**
-     * @dataProvider inputProvider
+     * @dataProvider firstInputProvider
      */
     public function testSolve(string $input, int $expectedFrequency): void
     {
@@ -19,12 +19,32 @@ class Day1SolutionTest extends TestCase
         $this->assertSame($expectedFrequency, $solution->solve($input));
     }
 
-    public function inputProvider()
+    public function firstInputProvider()
     {
         return [
             ['+1 +1 +1', 3],
             ['+1 +1 -2', 0],
             ['-1 -2 -3', -6],
+        ];
+    }
+
+    /**
+     * @dataProvider secondInputProvider
+     */
+    public function testSolveSecondPart(string $input, int $expectedFrequency): void
+    {
+        $solution = new Day1Solution($input);
+
+        $this->assertSame($expectedFrequency, $solution->solveSecondPart($input));
+    }
+
+    public function secondInputProvider()
+    {
+        return [
+            ['+1 -1', 0],
+            ['+3 +3 +4 -2 -4', 10],
+            ['-6 +3 +8 +5 -6', 5],
+            ['+7 +7 -2 -7 -4', 14],
         ];
     }
 }
