8a820f9fd83401293ce71de2943c56917dd9e33b
Alessandro Lai <alessandro.lai85@gmail.com>
2018-12-11 23:37:08 +0100

    Add (terribly slow) second solution to day 9

--- src/Xmas2018/Day9/Day9Solution.php
+++ src/Xmas2018/Day9/Day9Solution.php
@@ -4,13 +4,22 @@ declare(strict_types=1);
 
 namespace Jean85\AdventOfCode\Xmas2018\Day9;
 
+use Jean85\AdventOfCode\SecondPartSolutionInterface;
 use Jean85\AdventOfCode\SolutionInterface;
 
-class Day9Solution implements SolutionInterface
+class Day9Solution implements SolutionInterface, SecondPartSolutionInterface
 {
     public function solve()
     {
+        return '';
         $marbleGame = new MarbleGame(418, 71339);
+
+        return $marbleGame->play();
+    }
+
+    public function solveSecondPart()
+    {
+        $marbleGame = new MarbleGame(418, 7133900);
 
         return $marbleGame->play();
     }
