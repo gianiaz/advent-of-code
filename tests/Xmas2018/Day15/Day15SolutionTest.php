cd101ab01d2c38daaeb4d179f89566c20d76bc2b
Alessandro Lai <alessandro.lai85@gmail.com>
2018-12-19 22:57:40 +0100

    Add (slow) solution for day 15, part one

---
+++ tests/Xmas2018/Day15/Day15SolutionTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Xmas2018\Day15;
+
+use Jean85\AdventOfCode\Xmas2018\Day15\Day15Solution;
+use PHPUnit\Framework\TestCase;
+
+class Day15SolutionTest extends TestCase
+{
+    /**
+     * @dataProvider solveDataProvider
+     */
+    public function testSolve(string $input, int $outcome): void 
+    {
+        $solution = new Day15Solution($input);
+        
+        $this->assertSame($outcome, $solution->solve());
+    }
+
+    public function solveDataProvider()
+    {
+        return [
+            [
+                '#######
+#.G...#
+#...EG#
+#.#.#G#
+#..G#E#
+#.....#
+#######',
+                27730,
+            ],
+            [
+                '#######
+#G..#E#
+#E#E.E#
+#G.##.#
+#...#E#
+#...E.#
+#######',
+                36334,
+            ]
+        ];
+    }
+}
