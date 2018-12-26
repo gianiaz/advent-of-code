5e1aafd19a89ee25bed8f70b758a895e0fe1163e
Alessandro Lai <alessandro.lai85@gmail.com>
2018-12-26 01:15:35 +0100

    Try to rework flowing loginc

--- src/Xmas2018/Day17/Underground.php
+++ src/Xmas2018/Day17/Underground.php
@@ -9,7 +9,7 @@ class Underground
     public const CLAY = '#';
     public const SAND = '.';
     public const STILL_WATER = '~';
-    public const FLOWED_WATER = '|';
+    public const FLOWING_WATER = '|';
 
     /** @var string[][] */
     private $map;
@@ -36,34 +36,131 @@ class Underground
         $this->initMap($input);
     }
 
-    public function flow(): bool
+    public function flow(int $x = 500, int $y = 0): bool
     {
-        $x = 500;
-        $y = 0;
-
         // flow down
-        while ($this->contains($x, $y + 1, [self::SAND, self::FLOWED_WATER])) {
-            $y++;
+        while ($this->contains($x, $y + 1, [self::SAND, self::FLOWING_WATER])) {
+            ++$y;
             if ($y > $this->maxY) {
                 return false;
             }
 
-            $this->map[$y][$x] = self::FLOWED_WATER;
+            $this->map[$y][$x] = self::FLOWING_WATER;
         }
-        
-        $originalFlowX = $x;
 
-        if ($this->contains($x, $y + 1, [self::STILL_WATER])) {
-            $y++;
-            // flow left
-            while ($this->contains($x, $y, [self::STILL_WATER])) {
-                $x--;
+        if ($this->hasClayOnBothSides($x, $y)) {
+            return $this->fillToTheLeft($x, $y) || $this->fillToTheRight($x, $y) || $this->putTheFinalStillWater($x, $y);
+        }
+
+        return $this->overflowToTheLeft($x, $y) || $this->overflowToTheRight($x, $y);
+    }
+
+    private function hasClayOnBothSides(int $startX, int $y): bool
+    {
+        $x = $startX;
+        while ($this->contains(--$x, $y, [self::SAND, self::FLOWING_WATER, self::STILL_WATER])) {
+            if ($this->contains($x, $y + 1, [self::FLOWING_WATER, self::SAND])) {
+                return false;
             }
         }
 
-        $this->map[$y][$x] = self::STILL_WATER;
+        if (! $this->contains($x, $y, [self::CLAY])) {
+            return false;
+        }
 
-        return true;
+        $x = $startX;
+        while ($this->contains(++$x, $y, [self::SAND, self::FLOWING_WATER, self::STILL_WATER])) {
+            if ($this->contains($x, $y + 1, [self::FLOWING_WATER, self::SAND])) {
+                return false;
+            }
+        }
+
+        return $this->contains($x, $y, [self::CLAY]);
+    }
+
+    private function fillToTheLeft(int $x, int $y): bool
+    {
+        if ($this->contains($x - 1, $y, [self::CLAY])) {
+            return false;
+        }
+
+        while ($this->contains(--$x, $y, [self::SAND, self::FLOWING_WATER])) {
+            $this->map[$y][$x] = self::FLOWING_WATER;
+        }
+
+        if ($this->contains($x, $y, [self::CLAY, self::STILL_WATER])) {
+            $this->map[$y][$x + 1] = self::STILL_WATER;
+
+            return true;
+        }
+
+        // otherwise it's sand, overflow
+        $this->map[$y][$x] = self::FLOWING_WATER;
+
+        return false;
+    }
+
+    private function fillToTheRight(int $x, int $y): bool
+    {
+        if ($this->contains($x + 1, $y, [self::CLAY])) {
+            return false;
+        }
+
+        while ($this->contains(++$x, $y, [self::SAND, self::FLOWING_WATER])) {
+            $this->map[$y][$x] = self::FLOWING_WATER;
+        }
+
+        if ($this->contains($x, $y, [self::CLAY, self::STILL_WATER])) {
+            $this->map[$y][$x - 1] = self::STILL_WATER;
+
+            return true;
+        }
+
+        // otherwise it's sand, overflow
+        $this->map[$y][$x] = self::FLOWING_WATER;
+
+        return false;
+    }
+
+    private function putTheFinalStillWater(int $x, int $y): bool
+    {
+        if ($this->contains($x, $y, [self::STILL_WATER, self::CLAY])) {
+            $this->map[$y][$x] = self::STILL_WATER;
+
+            return true;
+        }
+
+        throw new \RuntimeException('WTF');
+    }
+
+    private function overflowToTheLeft(int $x, int $y): bool
+    {
+        while ($this->contains($x, $y + 1, [self::CLAY, self::STILL_WATER]) && $this->contains($x, $y, [self::SAND, self::FLOWING_WATER])) {
+            $this->map[$y][$x--] = self::FLOWING_WATER;
+        }
+
+        if ($this->contains($x, $y, [self::CLAY])) {
+            return false;
+        }
+
+        $this->map[$y][$x] = self::FLOWING_WATER;
+
+        return $this->flow($x, $y);
+    }
+
+    private function overflowToTheRight(int $x, int $y): bool
+    {
+        while ($this->contains($x, $y + 1, [self::CLAY, self::STILL_WATER]) && $this->contains($x, $y, [self::SAND, self::FLOWING_WATER])) {
+            $this->map[$y][$x++] = self::FLOWING_WATER;
+        }
+
+        if ($this->contains($x, $y, [self::CLAY])) {
+            return false;
+        }
+
+        $this->map[$y][$x] = self::FLOWING_WATER;
+
+        return $this->flow($x, $y);
     }
 
     public function getActualSituation(): string
