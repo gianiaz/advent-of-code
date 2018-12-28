6416fe79fb4332877313a66191d330f48ecf0d34
Alessandro Lai <alessandro.lai85@gmail.com>
2018-12-28 09:03:40 +0100

    Add helper code to solve both of day 19

---
+++ solve19.php
@@ -0,0 +1,12 @@
+<?php
+
+use Jean85\AdventOfCode\SecondPartSolutionInterface;
+use Jean85\AdventOfCode\SolutionInterface;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$solution = new \Jean85\AdventOfCode\Xmas2018\Day19\Day19Solution();
+
+$solution->runOptmimized();
+
+echo PHP_EOL;
--- src/Xmas2018/Day19/Day19Solution.php
+++ src/Xmas2018/Day19/Day19Solution.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Jean85\AdventOfCode\Xmas2018\Day19;
 
+use Jean85\AdventOfCode\SecondPartSolutionInterface;
 use Jean85\AdventOfCode\SolutionInterface;
 use Jean85\AdventOfCode\Xmas2018\Day16\Opcode\AbstractWorkingOpcode;
 use Jean85\AdventOfCode\Xmas2018\Day16\Opcode\Addition\Addi;
@@ -23,13 +24,16 @@ use Jean85\AdventOfCode\Xmas2018\Day16\Opcode\GreaterThan\Gtrr;
 use Jean85\AdventOfCode\Xmas2018\Day16\Opcode\Multiplication\Muli;
 use Jean85\AdventOfCode\Xmas2018\Day16\Opcode\Multiplication\Mulr;
 
-class Day19Solution implements SolutionInterface
+class Day19Solution implements SolutionInterface, SecondPartSolutionInterface
 {
     /** @var Opcode[] */
     private $opcodes;
 
     /** @var int */
-    private $ip;
+    private $instructionPointer;
+
+    /** @var int */
+    private $ipBoundTo;
 
     /** @var int[] */
     private $registers;
@@ -37,21 +41,81 @@ class Day19Solution implements SolutionInterface
     /** @var AbstractWorkingOpcode[] */
     private $workers;
 
-    public function __construct(array $opcodes = null, int $ip = 3)
+    public function __construct(array $opcodes = null, int $ipBoundTo = 3)
     {
         $this->opcodes = $opcodes ?? $this->getInput();
-        $this->ip = $ip;
+        $this->instructionPointer = 0;
+        $this->ipBoundTo = $ipBoundTo;
         $this->registers = \array_fill(0, 6, 0);
         $this->workers = $this->initWorkers();
     }
 
     public function solve()
     {
+        $count = 0;
         do {
-            echo $this->ip . ': ' . implode(' ', $this->registers) . PHP_EOL;
-        } while ($this->step());
-        
+            echo $count . ' -- ' . $this->instructionPointer . ":\t" . implode("\t", $this->registers) . PHP_EOL;
+
+            $this->step();
+        } while ($count++ <= 0);
+
         return $this->registers[0];
+    }
+
+    public function solveSecondPart()
+    {
+        $count = 0;
+        $this->registers[0] = 1;
+        do {
+            echo $count . ' -- ' . $this->instructionPointer . ":\t" . implode("\t", $this->registers) . PHP_EOL;
+
+            $this->step();
+        } while ($count++ <= 50);
+
+        return $this->registers[0];
+    }
+
+    public function runNormally(int $upUntilSteps)
+    {
+        $count = 0;
+        do {
+            echo $this->instructionPointer . ': ' . implode("\t", $this->registers) . PHP_EOL;
+            if ($count++ > $upUntilSteps) {
+                return $this->registers;
+            }
+        } while ($this->step());
+
+        return $this->registers[0];
+    }
+
+    public function runOptmimized(int $upUntilSteps = 738000000000)
+    {
+        $count = 21;
+        $this->registers = [0, 0, 919, 2, 1, 2];
+        $this->instructionPointer = 3;
+        
+        do {
+            echo $count . ' -- ' . $this->instructionPointer . ': ' . implode("\t", $this->registers) . PHP_EOL;
+            if ($this->registers[4] * $this->registers[5] === 919) {
+                $this->registers[0]++;
+                $this->registers[4]++;
+                $this->registers[5] = 1;
+                $count += 11;
+                
+                continue;
+            }
+
+            if ($this->registers[4] * $this->registers[5] !== 919) {
+                $this->registers[5]++;
+                $count += 8;
+                continue;
+            }
+
+            $this->step();
+            $count++;
+        } while($count <= $upUntilSteps);
+        
+        
     }
 
     /**
@@ -64,7 +128,7 @@ class Day19Solution implements SolutionInterface
 
     private function getCurrentInstruction(): ?Opcode
     {
-        return $this->opcodes[$this->ip] ?? null;
+        return $this->opcodes[$this->instructionPointer] ?? null;
     }
 
     public function step(): bool
@@ -75,10 +139,10 @@ class Day19Solution implements SolutionInterface
             return false;
         }
 
-        $this->registers[0] = $this->ip;
+        $this->registers[$this->ipBoundTo] = $this->instructionPointer;
         $this->execute($currentInstruction);
-        $this->ip = $this->registers[0];
-        ++$this->ip;
+        $this->instructionPointer = $this->registers[$this->ipBoundTo];
+        ++$this->instructionPointer;
 
         return true;
     }
@@ -114,26 +178,26 @@ class Day19Solution implements SolutionInterface
     private function getInput(): array
     {
         return [
-            new Opcode('addi', 3, 16, 3),
-            new Opcode('seti', 1, 8, 4),
-            new Opcode('seti', 1, 4, 5),
-            new Opcode('mulr', 4, 5, 1),
-            new Opcode('eqrr', 1, 2, 1),
-            new Opcode('addr', 1, 3, 3),
-            new Opcode('addi', 3, 1, 3),
-            new Opcode('addr', 4, 0, 0),
-            new Opcode('addi', 5, 1, 5),
-            new Opcode('gtrr', 5, 2, 1),
-            new Opcode('addr', 3, 1, 3),
-            new Opcode('seti', 2, 1, 3),
-            new Opcode('addi', 4, 1, 4),
-            new Opcode('gtrr', 4, 2, 1),
-            new Opcode('addr', 1, 3, 3),
-            new Opcode('seti', 1, 3, 3),
-            new Opcode('mulr', 3, 3, 3),
-            new Opcode('addi', 2, 2, 2),
-            new Opcode('mulr', 2, 2, 2),
-            new Opcode('mulr', 3, 2, 2),
+            new Opcode('addi', 3, 16, 3), // 0
+            new Opcode('seti', 1, 8, 4), // 1
+            new Opcode('seti', 1, 4, 5), // 2
+            new Opcode('mulr', 4, 5, 1), // 3
+            new Opcode('eqrr', 1, 2, 1), // 4
+            new Opcode('addr', 1, 3, 3), // 5
+            new Opcode('addi', 3, 1, 3), // 6
+            new Opcode('addr', 4, 0, 0), // 7
+            new Opcode('addi', 5, 1, 5), // 8
+            new Opcode('gtrr', 5, 2, 1), // 9
+            new Opcode('addr', 3, 1, 3), // 10
+            new Opcode('seti', 2, 1, 3), // 11
+            new Opcode('addi', 4, 1, 4), // 12
+            new Opcode('gtrr', 4, 2, 1), // 13
+            new Opcode('addr', 1, 3, 3), // 14
+            new Opcode('seti', 1, 3, 3), // 15
+            new Opcode('mulr', 3, 3, 3), // 16
+            new Opcode('addi', 2, 2, 2), // 17
+            new Opcode('mulr', 2, 2, 2), // 18
+            new Opcode('mulr', 3, 2, 2), // 19
             new Opcode('muli', 2, 11, 2),
             new Opcode('addi', 1, 3, 1),
             new Opcode('mulr', 1, 3, 1),
