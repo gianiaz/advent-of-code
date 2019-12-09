fa4776541d489288bbb5f40a943ba21db806535d
Alessandro Lai <alessandro.lai85@gmail.com>
2019-12-09 10:32:24 +0100

    Implement first tests plus a broken one

--- tests/Xmas2019/Day9/AdjustRelativeBaseTest.php
+++ tests/Xmas2019/Day9/AdjustRelativeBaseTest.php
@@ -22,4 +22,35 @@ class AdjustRelativeBaseTest extends TestCase
 
         $this->assertSame(1985, $memory->getOutput());
     }
+
+    public function testQuineWithVoidOffsets(): void
+    {
+        $memory = new MemoryWithRelativeMode([109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]);
+
+        $computer = (new Day9Solution())->creatComputer();
+        $computer->run($memory);
+
+        $this->assertSame(99, $memory->getOutput());
+    }
+
+    public function testWithLongOutput(): void
+    {
+        $memory = new MemoryWithRelativeMode([1102, 34915192, 34915192, 7, 4, 7, 99, 0]);
+
+        $computer = (new Day9Solution())->creatComputer();
+        $computer->run($memory);
+
+        $this->assertSame(16, strlen((string) $memory->getOutput()));
+    }
+
+    public function testWithLargeInput(): void
+    {
+        $memory = new MemoryWithRelativeMode([104, 1125899906842624, 99]);
+
+        $computer = (new Day9Solution())->creatComputer();
+        $computer->run($memory);
+
+        $this->assertSame(1125899906842624, $memory->getOutput());
+        $this->assertSame('1125899906842624', (string) $memory->getOutput());
+    }
 }
