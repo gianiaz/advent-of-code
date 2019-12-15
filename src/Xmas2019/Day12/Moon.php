1f7852ae5b650c2c868103c0dbe4e9f9a3395465
Alessandro Lai <alessandro.lai85@gmail.com>
2019-12-15 23:11:03 +0100

    Implement moons with gravity and velocity

---
+++ src/Xmas2019/Day12/Moon.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jean85\AdventOfCode\Xmas2019\Day12;
+
+class Moon
+{
+    /** @var Coordinates */
+    private $position;
+
+    /** @var Coordinates */
+    private $velocity;
+
+    public function __construct(int $x, int $y, int $z)
+    {
+        $this->position = new Coordinates($x, $y, $z);
+        $this->velocity = new Coordinates(0, 0, 0);
+    }
+
+    public function getPosition(): Coordinates
+    {
+        return $this->position;
+    }
+
+    public function getVelocity(): Coordinates
+    {
+        return $this->velocity;
+    }
+
+    public function applyGravity(Moon $moon2): void
+    {
+        $axys = ['x', 'y', 'z'];
+
+        foreach ($axys as $a) {
+            if ($this->position->$a > $moon2->position->$a) {
+                ++$this->velocity->$a;
+                --$moon2->velocity->$a;
+            }
+
+            if ($this->position->$a < $moon2->position->$a) {
+                --$this->velocity->$a;
+                ++$moon2->velocity->$a;
+            }
+        }
+    }
+
+    public function applyVelocity(): void
+    {
+        $this->position->x += $this->velocity->x;
+        $this->position->y += $this->velocity->y;
+        $this->position->z += $this->velocity->z;
+    }
+}
---
+++ tests/Xmas2019/Day12/MoonTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Xmas2019\Day12;
+
+use Jean85\AdventOfCode\Xmas2019\Day12\Coordinates;
+use Jean85\AdventOfCode\Xmas2019\Day12\Moon;
+use PHPUnit\Framework\TestCase;
+
+class MoonTest extends TestCase
+{
+    public function testApplyGravity(): void
+    {
+        $moon1 = new Moon(3, 0, 0);
+        $moon2 = new Moon(5, 0, 0);
+
+        $moon1->applyGravity($moon2);
+
+        $this->assertEquals(new Coordinates(-1, 0, 0), $moon1->getVelocity());
+        $this->assertEquals(new Coordinates(1, 0, 0), $moon2->getVelocity());
+    }
+
+    public function testApplyGravityIsCommutative(): void
+    {
+        $moon1 = new Moon(3, 0, 0);
+        $moon2 = new Moon(5, 0, 0);
+
+        $moon2->applyGravity($moon1);
+
+        $this->assertEquals(new Coordinates(-1, 0, 0), $moon1->getVelocity());
+        $this->assertEquals(new Coordinates(1, 0, 0), $moon2->getVelocity());
+    }
+
+    public function testApplyGravityWorksOnAllAxys(): void
+    {
+        $moon1 = new Moon(3, 0, 5);
+        $moon2 = new Moon(5, 0, 3);
+
+        $moon2->applyGravity($moon1);
+
+        $this->assertEquals(new Coordinates(-1, 0, 1), $moon1->getVelocity());
+        $this->assertEquals(new Coordinates(1, 0, -1), $moon2->getVelocity());
+    }
+
+    public function testApplyVelocity(): void
+    {
+        $moon = new Moon(3, 0, 5);
+
+        $moon->applyVelocity();
+
+        $this->assertEquals(new Coordinates(3, 0, 5), $moon->getPosition());
+
+        $moon->getVelocity()->x = 2;
+        $moon->getVelocity()->y = -2;
+        $moon->getVelocity()->z = 1;
+
+        $moon->applyVelocity();
+
+        $this->assertEquals(new Coordinates(5, -2, 6), $moon->getPosition());
+    }
+}
