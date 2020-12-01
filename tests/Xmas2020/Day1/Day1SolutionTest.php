f10e1584b35a5be8388d0743b7fefdfac09a9f99
Alessandro Lai <alessandro.lai85@gmail.com>
2020-12-01 22:09:27 +0100

    Solve day 1 of 2020

---
+++ tests/Xmas2020/Day1/Day1SolutionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Xmas2020\Day1;
+
+use Jean85\AdventOfCode\Xmas2020\Day1\Day1Solution;
+use PHPUnit\Framework\TestCase;
+
+class Day1SolutionTest extends TestCase
+{
+    /**
+     * @dataProvider inputProvider
+     */
+    public function test(array $input): void
+    {
+        $day1Solution = new Day1Solution();
+
+        $this->assertSame(514579, $day1Solution->solve($input));
+    }
+
+    public function inputProvider(): array
+    {
+        return [
+            [[1721, 299]],
+            [[1721, 979, 366, 299, 675, 1456]],
+        ];
+    }
+}
