4724fa2a227ffb4ed2ff8b79467462b1bb6e6f80
Alessandro Lai <alessandro.lai85@gmail.com>
2021-12-09 12:49:35 +0100

    Add more tests for day 6, still unsolved (too slow)

--- tests/Xmas2021/Day6/Day6SolutionTest.php
+++ tests/Xmas2021/Day6/Day6SolutionTest.php
@@ -11,6 +11,53 @@ class Day6SolutionTest extends TestCase
 {
     private const TEST_INPUT = '3,4,3,1,2';
 
+    /**
+     * @dataProvider projectionSmallDataProvider
+     */
+    public function testProjectionInALimitedSet(int $expected, int $start, int $days): void
+    {
+        $day6Solution = new Day6Solution();
+
+        $this->assertSame($expected, $day6Solution->projectFishes((string)$start, $days));
+    }
+
+    /**
+     * @return array{int, int, int}[]
+     */
+    public function projectionSmallDataProvider(): array
+    {
+        return [
+            [1, 6, 6],
+            [2, 6, 7],
+            [2, 6, 13],
+            [3, 6, 14],
+            [3, 6, 15],
+            [4, 6, 16],
+            [4, 6, 20],
+            [5, 6, 21],
+        ];
+    }
+
+    /**
+     * @dataProvider projectionDataProvider
+     */
+    public function testProjection(int $days, int $expectedProjection): void
+    {
+        $day6Solution = new Day6Solution();
+
+        $this->assertSame($expectedProjection, $day6Solution->projectFishes(self::TEST_INPUT, $days));
+    }
+
+    /**
+     * @return array{int,int}[]
+     */
+    public function projectionDataProvider(): array
+    {
+        return [
+            [18, 26],
+        ];
+    }
+
     public function test(): void
     {
         $day6Solution = new Day6Solution();
@@ -22,7 +69,6 @@ class Day6SolutionTest extends TestCase
     {
         $day6Solution = new Day6Solution();
 
-        $this->markTestIncomplete();
-        $this->assertSame(12, $day6Solution->solveSecondPart(self::TEST_INPUT));
+        $this->assertSame(26984457539, $day6Solution->solveSecondPart(self::TEST_INPUT));
     }
 }
