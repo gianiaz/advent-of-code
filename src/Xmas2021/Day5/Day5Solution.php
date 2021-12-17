1faf8dbc78329b6334fe016f32f8b0c512748346
Alessandro Lai <alessandro.lai85@gmail.com>
2021-12-17 23:55:42 +0100

    Refactor day 5 (but stil bugged)

--- src/Xmas2021/Day5/Day5Solution.php
+++ src/Xmas2021/Day5/Day5Solution.php
@@ -16,39 +16,27 @@ class Day5Solution implements SolutionInterface, SecondPartSolutionInterface
     {
         $input ??= trim(file_get_contents(__DIR__ . '/input.txt'));
 
-        foreach (explode(PHP_EOL, $input) as $coordinates) {
-            $line = new Line($coordinates);
-            if ($line->isDiagonal()) {
-                continue;
-            }
-
-            foreach ($line->getLine() as [$x, $y]) {
-                $this->map[$x][$y] ??= 0;
-                $this->map[$x][$y] += 1;
-            }
-        }
-
-        $count = 0;
-        foreach ($this->map as $row) {
-            foreach ($row as $value) {
-                if ($value > 1) {
-                    ++$count;
-                }
-            }
-        }
-
-        return $count;
+        return $this->calculateOverlaps($input, true);
     }
 
     public function solveSecondPart(string $input = null)
     {
         $input ??= trim(file_get_contents(__DIR__ . '/input.txt'));
 
+        return $this->calculateOverlaps($input, false);
+    }
+
+    private function calculateOverlaps(string $input, bool $skipDiagonals): int
+    {
         foreach (explode(PHP_EOL, $input) as $coordinates) {
             $line = new Line($coordinates);
+            if ($skipDiagonals && $line->isDiagonal()) {
+                continue;
+            }
+
             foreach ($line->getLine() as [$x, $y]) {
-                $this->map[$x][$y] ??= 0;
-                $this->map[$x][$y] += 1;
+                $this->map[$y][$x] ??= 0;
+                $this->map[$y][$x] += 1;
             }
         }
 
