b8ae6934f449c57ddac16dd6b2625df5a62f4626
Alessandro Lai <alessandro.lai85@gmail.com>
2023-12-30 22:57:50 +0100

    Solve day 9, part 2

--- src/Xmas2023/Day9/Day9Solution.php
+++ src/Xmas2023/Day9/Day9Solution.php
@@ -14,21 +14,36 @@ class Day9Solution implements SolutionInterface, SecondPartSolutionInterface
     {
         $input ??= Input::read(__DIR__);
 
-        $solution = 0;
-        foreach (explode(PHP_EOL, $input) as $row) {
-            $values = explode(' ', $row);
-            $solution += $this->interpolateNextStep(...array_map(static fn(string $a): int => (int) $a, $values));
-        }
-
-        return (string) $solution;
+        return (string) $this->getSolution($this->interpolateNextStep(...), $input);
     }
 
     public function solveSecondPart(string $input = null): string
     {
         $input ??= Input::read(__DIR__);
-        $solution = 0;
 
-        return (string) $solution;
+        return (string) $this->getSolution($this->interpolatePrevStep(...), $input);
+    }
+
+    protected function getSolution(callable $nextStep, string $input): int
+    {
+        $solution = 0;
+        foreach (explode(PHP_EOL, $input) as $row) {
+            $values = explode(' ', $row);
+            $solution += $nextStep(...array_map(static fn(string $a): int => (int) $a, $values));
+        }
+
+        return $solution;
+    }
+    private function interpolatePrevStep(int ...$values): int
+    {
+        if ($this->itsAllZeroes($values)) {
+            return 0;
+        }
+
+        $stepBelow = $this->getStepBelow($values);
+
+        return array_shift($values)
+            - $this->interpolatePrevStep(...$stepBelow);
     }
 
     private function interpolateNextStep(int ...$values): int
@@ -37,11 +52,7 @@ class Day9Solution implements SolutionInterface, SecondPartSolutionInterface
             return 0;
         }
 
-        $stepBelow = [];
-        $iMax = count($values);
-        for ($i = 1; $i < $iMax; ++$i) {
-            $stepBelow[] = $values[$i] - $values[$i - 1];
-        }
+        $stepBelow = $this->getStepBelow($values);
 
         return array_pop($values)
             + $this->interpolateNextStep(...$stepBelow);
@@ -56,5 +67,16 @@ class Day9Solution implements SolutionInterface, SecondPartSolutionInterface
         }
 
         return true;
+    }
+
+    private function getStepBelow(array $values): array
+    {
+        $stepBelow = [];
+        $iMax = count($values);
+        for ($i = 1; $i < $iMax; ++$i) {
+            $stepBelow[] = $values[$i] - $values[$i - 1];
+        }
+
+        return $stepBelow;
     }
 }
--- tests/Xmas2023/Day9/Day9SolutionTest.php
+++ tests/Xmas2023/Day9/Day9SolutionTest.php
@@ -11,20 +11,23 @@ class Day9SolutionTest extends TestCase
 {
     public function test(): void
     {
-        $input = '0 3 6 9 12 15
-1 3 6 10 15 21
-10 13 16 21 30 45';
         $Day9Solution = new Day9Solution();
 
-        $this->assertSame('114', $Day9Solution->solve($input));
+        $this->assertSame('114', $Day9Solution->solve($this->getInput()));
     }
 
     public function testSecondPart(): void
     {
-        $this->markTestIncomplete();
         $input = '';
         $Day9Solution = new Day9Solution();
 
-        $this->assertSame('6', $Day9Solution->solveSecondPart($input));
+        $this->assertSame('2', $Day9Solution->solveSecondPart($this->getInput()));
+    }
+
+    private function getInput(): string
+    {
+        return '0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45';
     }
 }
